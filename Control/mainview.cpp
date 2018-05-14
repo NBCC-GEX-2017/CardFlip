b9ea4ee846af9420105af6c594be99efe74d789c
Impy <vrowse95@gmail.com>
2018-05-14 11:13:48 -0300

    inital commit

---
+++ Control/mainview.cpp
@@ -0,0 +1,51 @@
+#include "mainview.h"
+#include "ui_mainview.h"
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QString>
+#include <QFont>
+
+MainView::MainView(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainView)
+{
+    ui->setupUi(this);
+
+    // set up model Deck deck; deck shuffle
+
+    // set up layouts
+    //  central widget -> vlMain -> hlCard
+    //                           -> hlShuffle
+    auto vlMain = new QVBoxLayout(ui->centralWidget);
+    auto hlCard = new QHBoxLayout();
+    vlMain->addLayout(hlCard);
+    auto hlShuffle = new QHBoxLayout();
+    vlMain->addLayout(hlShuffle);
+
+    ui->centralWidget->setStyleSheet(QStringLiteral("background-color:rgb(23,101,17"));
+
+    // set up cardButton
+    QFont font;
+    font.setPixelSize(40);
+    auto* card = new QPushButton;
+    card->setFont(font);
+    card->setMinimumSize(QSize(128,192));
+    card->setMaximumSize(QSize(128,192));
+    card->setText("");
+    card->setStyleSheet("border-image:url(:/media/Media/cardback.png)");
+
+    // set up shuffle button
+    auto shuffleButton = new QPushButton();
+    shuffleButton->setText("Shuffle");
+    shuffleButton->setMinimumSize(QSize(128,192));
+    shuffleButton->setMaximumSize(QSize(128,192));
+    shuffleButton->setStyleSheet(QStringLiteral("border-color:lightsteelblue"));
+    hlShuffle->addWidget(shuffleButton);
+
+}
+
+MainView::~MainView()
+{
+    delete ui;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "mainview.h"
+#include "Control/mainview.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
