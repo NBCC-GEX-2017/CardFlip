16e36f8ccd6873972505a78ef85b04370de8e41c
COFFEEISLIFE\melan <melanieroyplommer@gmail.com>
2018-05-15 14:13:52 -0300

    shuffle and cardDisplayBtn

--- Control/mainview.cpp
+++ Control/mainview.cpp
@@ -6,13 +6,18 @@
 #include <QString>
 #include <QFont>
 
+
 MainView::MainView(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainView)
 {
-    ui->setupUi(this);
-    //auto* grid = new QGridLayout(ui->centralWidget);
-    // set up model Deck deck; deck.shuffle();
+    ui->setupUi(this);    
+
+    deck = std::unique_ptr<Deck>(new Deck);
+    deck->shuffle();
+
+    // add gridlayout **
+
 
     // set up layouts
 
@@ -28,20 +33,26 @@ MainView::MainView(QWidget *parent) :
 
     //                          -> hlShuffle
 
-    ui->centralWidget->setStyleSheet(QStringLiteral("background-color:rgb(23, 101, 17"));
+    ui->centralWidget->setStyleSheet(QStringLiteral("background-color:rgb(23, 101, 17);"));
 
     // set up cardButton
     QFont font;
     font.setPixelSize(40);
 
 
-    auto* card = new QPushButton;
-    card->setFont(font);
-    card->setMinimumSize(QSize(128,192));
-    card->setMaximumSize(QSize(128,192));
-    card->setText("");
-    card->setStyleSheet("border-image:url(:/new/Media/Media/cardback.png)");
-    hlMain->addWidget(card); // add button to layout
+    cardDisplayBtn = new QPushButton;
+    cardDisplayBtn->setFont(font);
+    cardDisplayBtn->setMinimumSize(QSize(128,192));
+    cardDisplayBtn->setMaximumSize(QSize(128,192));
+    cardDisplayBtn->setText("");
+    cardDisplayBtn->setStyleSheet("border-image:url(:/new/Media/Media/cardback.png)");
+    hlMain->addWidget(cardDisplayBtn); // add button to layout
+    hlMain->addStretch(1);
+
+    connect(cardDisplayBtn,
+            &QPushButton::clicked,
+            this,
+            &MainView::onCardClick);
 
     //connect(card, &QPushButton::clicked, this, &MainView::centralWidget);
 
@@ -54,11 +65,47 @@ MainView::MainView(QWidget *parent) :
     shuffle->setStyleSheet(QStringLiteral("background-color:lightsteelblue"));
 
     hlShuffle->addWidget(shuffle); // add button to layout
-    //connect(shuffle, &QPushButton::clicked, this, &MainView::centralWidget);
+    connect(shuffle,
+            &QPushButton::clicked,
+            this,
+            [this](){
+        deck->shuffle();
+        drawView();});
 
 }
 
 MainView::~MainView()
 {
     delete ui;
+}
+
+void MainView::onCardClick(){
+    //deck->nextCard();
+    drawView();
+}
+
+void MainView::drawView(){
+
+    if(deck->isFlipped())
+    {
+        if(deck->getCardColor()== CardColor::Red){
+           cardDisplayBtn->setStyleSheet("border-image:url(:/new/Media/Media/cardfront.png); "
+                                         "color: red;");
+        }
+        else{
+           cardDisplayBtn->setStyleSheet("border-image:url(:/new/Media/Media/cardfront.png); "
+                                         "color: black;");
+        }
+
+        cardDisplayBtn->setText(QString::fromStdString(deck->topCardToString()));
+        //cardDisplayBtn->setStyleSheet("color: red;");
+    }
+    else
+    {
+        cardDisplayBtn->setStyleSheet("border-image:url(:/new/Media/Media/cardback.png);");
+        cardDisplayBtn->setText("");
+    }
+
+
+
 }
--- Control/mainview.h
+++ Control/mainview.h
@@ -1,11 +1,16 @@
 #ifndef MAINVIEW_H
 #define MAINVIEW_H
 
+#include <Model/Card.h>
+#include <Model/Deck.h>
 #include <QMainWindow>
+#include <memory>
 
 namespace Ui {
 class MainView;
 }
+
+class QPushButton;
 
 class MainView : public QMainWindow
 {
@@ -15,8 +20,24 @@ public:
     explicit MainView(QWidget *parent = 0);
     ~MainView();
 
+private slots:
+    void onCardClick();
+
+//methods
+private:
+
+    void drawView();
+//members
 private:
     Ui::MainView *ui;
+
+    std::unique_ptr<Deck> deck;
+    /////////////////////////
+    QPushButton* cardDisplayBtn;
+ //change to a vector. to deal 32 cards in a grid...refer to demo03 (tic tac toe)
+
+
+
 };
 
 #endif // MAINVIEW_H
