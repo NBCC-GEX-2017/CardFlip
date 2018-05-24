50ca900877be8c4a001d23b31fa47c322195c278
QuangMinhLe <leminhquang9@gmail.com>
2018-05-24 10:31:48 -0300

    matchingGame class update

--- Model/matchinggame.cpp
+++ Model/matchinggame.cpp
@@ -1,6 +1,30 @@
 #include "matchinggame.h"
 
-matchingGame::matchingGame()
+matchingGame::matchingGame(int numberOfCards, Deck &deck)
 {
+    cards.reserve(numberOfCards);
+
+    for(int i=0; i < numberOfCards){
+        if(!deck.isEmpty())
+            cards.push_back(deck.drawCard());
+    }
 
 }
+
+CardPtr matchingGame::getCardN(int index)
+{
+    return cards.at(index);
+}
+
+void matchingGame::selectCardN(int index)
+{
+    if(index < cards.size()){
+        cards[index]->flip();
+        for(int i = 0; i < cards.size(); i++)
+        {
+            if(cards[i]->isFlipped()&&i!=index)
+                cards[i]->flip();
+        }
+    }
+}
+
