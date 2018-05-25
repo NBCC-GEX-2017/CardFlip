f3922139f15a26f6780e26249f829bc5f439391e
QuangMinhLe <leminhquang9@gmail.com>
2018-05-25 11:28:08 -0300

    update

--- Model/matchinggame.h
+++ Model/matchinggame.h
@@ -1,11 +1,21 @@
 #ifndef MATCHINGGAME_H
 #define MATCHINGGAME_H
 
+#include "Model/Card.h"
+#include "Model/Deck.h"
+#include <vector>
 
 class matchingGame
 {
 public:
-    matchingGame();
+    matchingGame(int numberOfCards, Deck& deck);
+
+    CardPtr getCardN(int index);
+    void selectCardN(int index);
+
+private:
+    std::vector<CardPtr> cards;
+
 };
 
 #endif // MATCHINGGAME_H
